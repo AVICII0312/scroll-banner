84a0a75e2b0251e1fedef50727a344d3b6ed857d
Acivii <Acivii0312@163.com>
2019-05-25 16:48:32 +0800

    修改jqCDN

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 var allButtons = $('#navbuttons > ul > li')
-for (let i = 0; i<allButtons.length;i++){
-  $(allButtons[i]).on('click',function(x){
+for (let i = 0; i < allButtons.length; i++) {
+  $(allButtons[i]).on('click', function (x) {
     $(x.currentTarget).addClass('active').siblings('.active').removeClass('active')
-    var p = $(x.currentTarget).index() *-920
+    var p = $(x.currentTarget).index() * -920
     $('#rollimages').css({
-        transform:'translate('+p+'px)'
+      transform: 'translate(' + p + 'px)'
     })
     console.log(p)
-    
+
   })
 }
